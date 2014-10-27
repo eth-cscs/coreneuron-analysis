d68783207f56dc02662393a64365c6537861b849
Ben Cumming <bcumming@cscs.ch>
2014-10-27 13:24:12 +0100

    more fixes

--- code/storage.cpp
+++ code/storage.cpp
@@ -1,11 +1,9 @@
 struct NrnThread {
   // linked list of Memb_list (mechanisms)
   NrnThreadMembList mechanisms;
-
   int ncell;     // number of cells
   int nnode;     // number of nodes
   double *data;  // what is this data? Pramod?
-
   double *rhs; // indexed by VEC_RHS
   double *d;   // indexed by VEC_D
   double *a;   // indexed by VEC_A
@@ -13,23 +11,25 @@ struct NrnThread {
   double *v;   // indexed by VEC_V
   int *parent_index; // indexed by p
 };
-
 // linked list for chaining 
 struct NrnThreadMembList {
   struct NrnThreadMembList *next;
   struct Memb_list *mech_data; // pointer to memb_list[]
   int index;                   // index for  memb_func[]
 };
-
+union Datum {
+    double val; int i;          // values
+    double* pval; void* pvoid; // pointers
+};
 // holds the data for a mechanism
 struct Memb_list { // nrnoc/nrnoc_ml.h
   // indexes of nodes with this mechanism
   // these index into matrix (i.e. VEC_*)
   int *nodeindices;
   double *data;
+  Datum  *pdata;
   int nodecount;
 };
-
 // function pointer for mechanism vtable implementation
 typedef void (*mod_f_t)(struct NrnThread *, Memb_list *, int);
 struct Memb_func { // nrnoc/membfunc.h
@@ -37,12 +37,9 @@ struct Memb_func { // nrnoc/membfunc.h
   mod_f_t jacob;
   mod_f_t state;
 }
-
-// NrnThread::mechanisms list indexes into here
-// declared in nrnoc/register_mech.c
+// NrnThread::mechanisms list indexes into here [nrnoc/register_mech.c]
 Memb_func* memb_func = malloc(memb_func_size*sizeof(Memb_func));
 Memb_list* memb_list = malloc(memb_func_size*sizeof(Memb_list));
-
 // example loop
 NrnThreadMembList* mech;
 for (mech = &thread->mechanisms; mech!=nullptr; mech = mech->next) {
